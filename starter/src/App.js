a7e02c775ddb7588805533fa72948076e167e24e
Ivy Ha <23127@stmarys.ac.uk>
2024-05-01 15:13:38 +0100

    creating own component for search function

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,16 +1,16 @@
 import "./App.css";
 import * as BooksAPI from "./BooksAPI";
+import {Link, Routes, Route} from "react-router-dom";
 import { useEffect, useState } from "react";
 
 //bookshelves 
 import Bookshelf from "./bookshelves/bookshelf";
+import SearchComponent from "./searchComponent/search";
 import Book from "./bookCreation/createBook";
 
 function App() {
   const [showSearchPage, setShowSearchpage] = useState(false);
   const [books, setBooks] = useState([]);
-  const [searchBooks, setSearchBooks] = useState([]);
-  const [query, setQuery] = useState(''); 
 
 
 // Fetch all books when the component mounts
@@ -21,30 +21,6 @@ useEffect(() => {
     }); 
 }, [showSearchPage]);
 
-// Perform a search whenever the query changes
-useEffect(() => {
-  if (query) {
-    BooksAPI.search(query, 20)
-      .then(searchedBooks => {
-        if (searchedBooks.error) {
-          setSearchBooks([]);
-        } else {
-          const mergeSearchBooks = searchedBooks.map(searchedBook => {
-            const findBook = books.find(book => book.id === searchedBook.id);
-            return {
-              ...searchedBook, 
-              shelf: findBook ? findBook.shelf : 'none' 
-            }
-          })
-
-          setSearchBooks(mergeSearchBooks);
-        }
-      });
-  } else {
-    setSearchBooks([books]); // Set searchBooks to current books state when query is empty
-  }
-}, [query, books]); //seperate useEffects to avoid unnecessary API calls
-
 const onShelfChange = (book, newShelf) => {
   BooksAPI.update(book, newShelf)
   .then(() => {
@@ -54,60 +30,19 @@ const onShelfChange = (book, newShelf) => {
     //update current book state on main page
     const updatedBooks = books.map(b => b.id === book.id ? updatedBook : b);
     setBooks(updatedBooks);
-
-    //update search book state on search page
-    const updatedSearchedBooks = searchBooks.map(b => b.id === book.id ? updatedBook : b);
-    setSearchBooks(updatedSearchedBooks);
-
   });
 }
 
-const handleChange = (e) => {
-  setQuery(e.target.value);
-}
-
-const search = query.trim().toLowerCase(); 
-let filteredBooks = searchBooks; 
-if (search.length > 0) {
-  filteredBooks = searchBooks.filter(book => {
-    return (
-      (book.title && book.title.toLowerCase().includes(search)) || 
-      (book.authors && book.authors.join('').toLowerCase().includes(search))
-    )
-  })
-}
 
   return (
     <div className="app">
       {showSearchPage ? (
-        <div className="search-books">
-          <div className="search-books-bar">
-            <a
-              className="close-search"
-              onClick={() => setShowSearchpage(!showSearchPage)}
-            >
-              Close
-            </a>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                placeholder="Search by title, author, or ISBN"
-                value={query}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-          
-          <div className="search-books-results">
-          <ol className="books-grid">
-            {query && filteredBooks.map(book => (
-              <li key={book.id}>
-                <Book book={book} onShelfChange={onShelfChange}/>
-              </li>
-            ))}
-          </ol>
-          </div>
-        </div>
+        <SearchComponent 
+          onShelfChange={onShelfChange} 
+          books={books} 
+          setShowSearchpage={setShowSearchpage} 
+          showSearchPage={showSearchPage} 
+        /> 
       ) : (
         <div className="list-books">
           <div className="list-books-title">
@@ -121,7 +56,7 @@ if (search.length > 0) {
             </div>
           </div>
           <div className="open-search">
-            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
+            <Link to="/search" onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</Link>
           </div>
         </div>
       )}
---
+++ starter/src/searchComponent/search.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from 'react';
+import * as BooksAPI from '../BooksAPI';
+import { Link } from 'react-router-dom';
+
+import Book from '../bookCreation/createBook';
+
+
+function SearchComponent ({onShelfChange, books, setShowSearchpage, showSearchPage}) {
+    const [searchBooks, setSearchBooks] = useState([]);
+    const [query, setQuery] = useState(''); 
+
+    useEffect(() => {
+        if (query) {
+          BooksAPI.search(query, 20)
+            .then(searchedBooks => {
+              if (searchedBooks.error) {
+                setSearchBooks([]);
+              } else {
+                const mergeSearchBooks = searchedBooks.map(searchedBook => {
+                  const findBook = books.find(book => book.id === searchedBook.id);
+                  return {
+                    ...searchedBook, 
+                    shelf: findBook ? findBook.shelf : 'none' 
+                  }
+                })
+      
+                setSearchBooks(mergeSearchBooks);
+              }
+            });
+        } else {
+          setSearchBooks([books]); // Set searchBooks to current books state when query is empty
+        }
+    }, [query, books]); //seperate useEffects to avoid unnecessary API calls
+
+    const onSearchShelfChange = (book, newShelf) => {
+        // Update searchBooks state
+        const updatedBook = {...book, shelf: newShelf};
+        const updatedSearchedBooks = searchBooks.map(b => b.id === book.id ? updatedBook : b);
+        setSearchBooks(updatedSearchedBooks);
+    
+        // Call App's onShelfChange function to update books state
+        onShelfChange(book, newShelf);
+      }
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    }
+    
+    const search = query.trim().toLowerCase(); 
+    let filteredBooks = searchBooks; 
+    if (search.length > 0) {
+        filteredBooks = searchBooks.filter(book => {
+            return (
+            (book.title && book.title.toLowerCase().includes(search)) || 
+            (book.authors && book.authors.join('').toLowerCase().includes(search))
+            )
+        })
+    }
+    return (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link
+              to="/"
+              className="close-search"
+              onClick={() => setShowSearchpage(!showSearchPage)}
+            >
+              Close
+            </Link>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+                value={query}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          
+          <div className="search-books-results">
+          <ol className="books-grid">
+            {query && filteredBooks.map(book => (
+              <li key={book.id}>
+                <Book book={book} onShelfChange={onSearchShelfChange}/>
+              </li>
+            ))}
+          </ol>
+          </div>
+        </div>
+    )
+
+}
+
+export default SearchComponent; 
